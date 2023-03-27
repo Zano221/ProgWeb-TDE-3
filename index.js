bcd8e0d8406aad551335027b86736fe683db4afe
Zano <zanopcwr@protonmail.com>
2023-03-27 11:34:48 -0300

    feat: finished!

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function login() {
-    const userName = document.getElementById("input-field")
-    let userNameLength = userName.value.length;
+    const inputField = document.getElementById("input-field")
+    let userNameLength = inputField.value.length;
     const errorMsg = document.getElementById("ErrorMsg")
+
+    errorMsg.textContent = "";
 
     if(userNameLength < 4) {
         errorMsg.textContent = "ERRO! Nome de usuáro DEVE ser maior que 3 caracteres!"
-    } else {
-        errorMsg.textContent = "";
+        return
     }
 
-    console.log(errorMsg)
+    let userName = inputField.value;
+    const hiddenElements = document.getElementsByClassName("hidden")
+
+    //forEach n funciona? :(
+    for(let i = 0; i < hiddenElements.length; i++) {
+        hiddenElements[i].style.display = "none";
+    }
+
+    const bemVindo = document.getElementById("bem-vindo");
+
+    bemVindo.innerText = "Bem vindo " + userName + "!";
+    bemVindo.style.display = "flex";
+
 }
